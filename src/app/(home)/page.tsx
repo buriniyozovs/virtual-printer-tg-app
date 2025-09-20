415358153eb0c243624f810c7775b86bfbbac261
buriniyozovs <sburiniyozov@students.wiut.uz>
2025-09-20 18:20:07 +0500

    commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,64 +1,9 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-
-// interface HomePageProps {
-// searchParams: Promise<{ [key: string]: string | undefined }>
-// }
-interface TelegramWebApp {
-  ready: () => void
-  expand: () => void
-  MainButton: {
-    text: string
-    color: string
-    show: () => void
-    hide: () => void
-  }
-  sendData: (data: string) => void
-  onEvent: (event: string, callback: () => void) => void
-  offEvent: (event: string, callback: () => void) => void
+import OrderUi from '@/containers/orders/OrderUi'
+interface HomePageProps {
+  searchParams: Promise<{ [key: string]: string | undefined }>
 }
-export default async function Home() {
-  // import { useEffect, useState } from 'react'
+export default async function Home({ searchParams }: HomePageProps) {
+  const resolvedSearchParams = await searchParams
 
-  // Define Telegram Web App types
-
-  const [telegram, setTelegram] = useState<TelegramWebApp | null>(null)
-
-  // Initialize Telegram WebApp
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-      const webApp = window.Telegram.WebApp
-      setTelegram(webApp)
-      webApp.ready()
-      webApp.expand()
-      webApp.MainButton.text = 'Send Hello'
-      webApp.MainButton.color = '#4CAF50'
-      webApp.MainButton.show()
-    }
-  }, [])
-
-  // Handle MainButton click
-  useEffect(() => {
-    if (telegram) {
-      const handleMainButtonClick = () => {
-        telegram.sendData('hello')
-      }
-
-      telegram.onEvent('mainButtonClicked', handleMainButtonClick)
-
-      return () => {
-        telegram.offEvent('mainButtonClicked', handleMainButtonClick)
-      }
-    }
-  }, [telegram])
-
-  return (
-    <div>hello</div>
-    // Component doesn't render any UI, as MainButton is controlled by Telegram WebApp
-  ) // Component doesn't render any UI, as MainButton is controlled by Telegram WebApp
+  return <OrderUi userId={resolvedSearchParams.userId} />
 }
-// const resolvedSearchParams = await searchParams
-
-// return <OrderUi userId={resolvedSearchParams.userId} />
-// }
