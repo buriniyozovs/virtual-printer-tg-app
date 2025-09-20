6ec70af59aa26bce58b3710af4f5216193772b1e
buriniyozovs <sburiniyozov@students.wiut.uz>
2025-09-20 18:04:00 +0500

    commit

--- src/containers/orders/OrderUi.tsx
+++ src/containers/orders/OrderUi.tsx
@@ -115,7 +115,7 @@ export default function OrderUi({ userId }: OrderUiProps) {
       console.log('Sending order data:', orderData)
 
       if (telegram) {
-        telegram.sendData(JSON.stringify(orderData))
+        telegram.sendData(JSON.stringify('test'))
       }
 
       // Reset form
