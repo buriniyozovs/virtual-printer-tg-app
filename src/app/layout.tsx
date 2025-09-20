1e949453f39640abffd17aa1198eb5db559cf8b6
buriniyozovs <sburiniyozov@students.wiut.uz>
2025-09-20 17:49:16 +0500

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import TelegramWrapper from '../containers/telegramWrapper/TelegramWrapper' // Adjust path as needed
 import './globals.css'
 
 const geistSans = Geist({
@@ -31,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TelegramWrapper>{children}</TelegramWrapper>
+        {children}
       </body>
     </html>
   )
