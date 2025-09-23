c262d96febaeb67e0120916f7bf4608c8d721d20
buriniyozovs <sburiniyozov@students.wiut.uz>
2025-09-23 18:30:22 +0500

    Updated backend url

--- src/services/featherClient.ts
+++ src/services/featherClient.ts
@@ -2,7 +2,7 @@ import { Application, feathers } from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
 import io from 'socket.io-client'
 
-const socket = io('https://1c2269d4c3da.ngrok-free.app', {
+const socket = io('https://f77d5aaf8e8e.ngrok-free.app', {
   transports: ['websocket'],
   forceNew: true,
 })
